f28c5ed2fbe4809c552d76ff8306fa703db09858
Jacob Barnes <jbarnes@barne.org>
2022-06-11 14:43:33 -0500

    Working on UI

---
+++ src/JACT.Desktop/Components/FilePicker.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace JACT.Desktop.Components;
+
+public partial class FilePicker : UserControl
+{
+    public FilePicker()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ src/JACT.Desktop/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace JACT.Desktop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
