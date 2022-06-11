d3394ba8923880b580f08b73280861dac563f85b
Jacob Barnes <jbarnes@barne.org>
2022-06-11 14:14:24 -0500

    Created project

---
+++ JACT/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace JACT;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ JACT/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace JACT;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
